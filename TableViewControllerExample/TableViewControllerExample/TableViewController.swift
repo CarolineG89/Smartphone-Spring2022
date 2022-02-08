78a8f336731ed344de8006a63b46aea4882ebd07
Xuemeng Gao <gao.xuem@northeastern.edu>
2022-02-07 22:37:26 -0800

    Checking in code for Custom Table View

---
+++ TableViewControllerExample/TableViewControllerExample/TableViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TableViewController.swift
+//  TableViewControllerExample
+//
+//  Created by Xuemeng Gao on 2/7/22.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    let arr = ["Mark", "Bill", "Tom", "Peter", "John"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arr.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+
+        // Configure the cell...
+        cell.textLabel?.text = arr[indexPath.row]
+        
+        // Adding some comment
+        
+        return cell
+    }
+    
+
+}
